b392f2c0d6cf8a70983a2422aa39ebec1fc61a26
Conor Brady <conorpbrady@gmail.com>
2022-09-08 08:00:50 -0400

    add basic delete confirmation modal

--- frontend/src/components/Accounts.js
+++ frontend/src/components/Accounts.js
@@ -1,6 +1,8 @@
 import React, { useState, useCallback } from 'react';
 import { submitNewAccount } from '../api/accountsApi';
 import { useGetAccounts } from '../hooks/useGetAccounts';
+import { ConfirmationModal } from './ConfirmationModal';
+import { Button } from 'react-bootstrap';
 
 export default function Accounts() {
   const initInputs = { accountName: '', accountType: 0 };
@@ -8,7 +10,26 @@ export default function Accounts() {
   const [inputs, setInputs] = useState(initInputs);
   const [fetchToggle, setFetchToggle] = useState(false);
 
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [id, setId] = useState(null);
+  const [type, setType] = useState(null);
+  const [deleteMessage, setDeleteMessage] = useState(null);
+
   const { accounts } = useGetAccounts(fetchToggle);
+
+  const displayConfirmationModal = (type, id) => {
+    setType(type);
+    setId(id);
+    setDeleteMessage(`Are you sure you want to delete ${type} ${id}`);
+    setShowConfirmationModal(true);
+  };
+
+  const hideConfirmationModal = () => {
+    setShowConfirmationModal(false);
+  };
+  const submitDelete = (type, id) => {
+    console.log('deleted');
+  };
 
   const handleChange = useCallback(({ target: { name, value } }) =>
     setInputs((prevState) => ({ ...prevState, [name]: value }), [])
@@ -31,7 +52,10 @@ export default function Accounts() {
           </tr>
         </thead>
         <tbody>
-          <AccountList accounts={accounts} />
+          <AccountList
+            accounts={accounts}
+            showDeleteModal={displayConfirmationModal}
+          />
         </tbody>
       </table>
 
@@ -57,6 +81,13 @@ export default function Accounts() {
 
         <input type="submit" value="Submit" />
       </form>
+      <ConfirmationModal showModal={showConfirmationModal}
+        confirmModal={submitDelete}
+        hideModal={hideConfirmationModal}
+        type={type}
+        id={id}
+        message={deleteMessage}
+      />
     </div>
   );
 }
@@ -67,6 +98,11 @@ function AccountList(props) {
       <tr key={index}>
         <td>{account.accountName}</td>
         <td>{account.accountType}</td>
+        <td>
+          <Button onClick={ () => props.showDeleteModal('Account', account.id) }>
+            Delete
+          </Button>
+        </td>
       </tr>
     );
   });
